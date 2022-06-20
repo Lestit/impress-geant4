0fd6c1dd66d6a3ffc8a34881d9a4f105218fdbf5
William Setterberg <william.s.setterberg@gmail.com>
2022-06-20 15:06:02 -0500

    another macro

--- helper-scripts/double-peak-analysis/generate_xyz_scan.py
+++ helper-scripts/double-peak-analysis/generate_xyz_scan.py
@@ -1,10 +1,14 @@
 import numpy as np
 
+OUT_FN = 'optical_xyz_scan.mac'
+
 LENGTH = 5
 Z_START = 3.75 - LENGTH/2
 Z_END = 3.75  + LENGTH/2
 NUM_Z = 10
 NUM_X = 10
+NUM_Y = 10
+RADIUS = 37/2 # mm
 NUM_PHOTS = 10000
 
 script_begin =\
@@ -23,13 +27,14 @@ f'''/gps/particle opticalphoton
 '''
 
 def pozition():
-    for x in np.linspace(0, 37/2, num=NUM_X):
-        for z in np.linspace(Z_START + 0.1, Z_END, num=NUM_Z):
-            yield (x, z)
+    for x in np.linspace(0, RADIUS, num=NUM_X):
+        for y in np.linspace(0, RADIUS, num=NUM_Y):
+            for z in np.linspace(Z_START + 0.1, Z_END, num=NUM_Z):
+                yield (x, y, z)
 
 if __name__ == '__main__':
-    with open('optical_vary_xz.mac', 'w') as f:
+    with open(OUT_FN, 'w') as f:
         print(script_begin, file=f)
-        for (x, z) in pozition():
-            print(f'/gps/pos/centre {x:.2f} 0 {z:.2f} mm', file=f)
+        for (x, y, z) in pozition():
+            print(f'/gps/pos/centre {x:.2f} {y:.2f} {z:.2f} mm', file=f)
             print(f'/run/beamOn {NUM_PHOTS}\n', file=f)
