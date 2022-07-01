fc9b97d199f280298b1e83645cd847271b547856
William Setterberg <william.s.setterberg@gmail.com>
2022-07-01 11:07:12 -0500

    change optical scan helper to cylindrical geometry

--- helper-scripts/double-peak-analysis/generate_xyz_scan.py
+++ helper-scripts/double-peak-analysis/generate_xyz_scan.py
@@ -8,9 +8,9 @@ POSITIONS_OUT_FN = pfx + 'positions-xyz-scan.txt'
 LENGTH = 5
 Z_START = 3.75 - LENGTH/2
 Z_END = 3.75  + LENGTH/2
+NUM_R = 10
+NUM_THETA = 10
 NUM_Z = 10
-NUM_X = 10
-NUM_Y = 10
 RADIUS = 37/2 # mm
 NUM_PHOTS = 10000
 
@@ -29,11 +29,11 @@ f'''/gps/particle opticalphoton
 '''
 
 def pozition():
-    max_xy = RADIUS / np.sqrt(2)
-    for x in np.linspace(0, max_xy - 0.5, num=NUM_X):
-        for y in np.linspace(0, max_xy - 0.5, num=NUM_Y):
+    ''' cylindrical position iterator over a quarter of the cylinder '''
+    for r in np.linspace(0, RADIUS - 0.5, num=NUM_R):
+        for theta in np.linspace(0, np.pi / 2, num=NUM_THETA):
             for z in np.linspace(Z_START + 0.5, Z_END - 0.5, num=NUM_Z):
-                yield (x, y, z)
+                yield (r * np.cos(theta), r * np.sin(theta), z)
 
 if __name__ == '__main__':
     with open(MACRO_OUT_FN, 'w') as macro_file, open(POSITIONS_OUT_FN, 'w') as pos_file:
